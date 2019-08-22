2c23874fd6389fd64e09118ce0eb221d4f3d64d6
Emily Kolar <emkolar@gmail.com>
2019-08-22 00:27:16 -0500

    rm unused, return of texts

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const format = require('date-format');
 const pretty = require('pretty-ms');
 const airports = require('airports');
 const puppeteer = require('puppeteer');
-const moment = require('moment');
 const qs = require('query-string');
 const { prompt } = require('enquirer');
 
@@ -400,61 +399,65 @@ async function getPrompts() {
     message: 'Setup SMS alerts?',
   }];
 
-  const {
-    originCode,
-    destCode,
-    deptDate,
-    retDate,
-    passCount,
-    price,
-    priceRound,
-    outTime,
-    inTime,
-    checkInt,
-    setupSms,
-  } = await prompt(questions);
-
-  adultPassengersCount = parseInt(passCount, 10);
-  departureDate = deptDate;
-  returnDate = retDate;
-  dealPriceThreshold = price ? parseFloat(price) : undefined;
-  dealPriceThresholdRoundTrip = priceRound ? parseFloat(priceRound) : undefined;
-  deptTime = outTime;
-  retTime = inTime;
-  originationAirportCode = originCode;
-  destinationAirportCode = destCode;
-  interval = parseInt(checkInt, 10);
-
-  if (setupSms) {
-    const questions2 = [{
-      type: 'input',
-      name: 'sid',
-      message: 'Twilio SID?',
-    }, {
-      type: 'input',
-      name: 'auth',
-      message: 'Twilio auth token?',
-    }, {
-      type: 'input',
-      name: 'fromNum',
-      message: 'From phone number? (Twilio number)',
-    }, {
-      type: 'input',
-      name: 'toNum',
-      message: 'To phone number? (Your number)',
-    }];
-
+  try {
     const {
-      sid,
-      auth,
-      fromNum,
-      toNum,
-    } = await prompt(questions2);
+      originCode,
+      destCode,
+      deptDate,
+      retDate,
+      passCount,
+      price,
+      priceRound,
+      outTime,
+      inTime,
+      checkInt,
+      setupSms,
+    } = await prompt(questions);
 
-    tSid = sid;
-    tFrom = fromNum;
-    tAuth = auth;
-    tTo = toNum;
+    adultPassengersCount = parseInt(passCount, 10);
+    departureDate = deptDate;
+    returnDate = retDate;
+    dealPriceThreshold = price ? parseFloat(price) : undefined;
+    dealPriceThresholdRoundTrip = priceRound ? parseFloat(priceRound) : undefined;
+    deptTime = outTime;
+    retTime = inTime;
+    originationAirportCode = originCode;
+    destinationAirportCode = destCode;
+    interval = parseInt(checkInt, 10);
+
+    if (setupSms) {
+      const questions2 = [{
+        type: 'input',
+        name: 'sid',
+        message: 'Twilio SID?',
+      }, {
+        type: 'input',
+        name: 'auth',
+        message: 'Twilio auth token?',
+      }, {
+        type: 'input',
+        name: 'fromNum',
+        message: 'From phone number? (Twilio number)',
+      }, {
+        type: 'input',
+        name: 'toNum',
+        message: 'To phone number? (Your number)',
+      }];
+
+      const {
+        sid,
+        auth,
+        fromNum,
+        toNum,
+      } = await prompt(questions2);
+
+      tSid = sid;
+      tFrom = fromNum;
+      tAuth = auth;
+      tTo = toNum;
+    }
+  } catch (e) {
+    console.log(e);
   }
 }
 
@@ -464,152 +467,154 @@ async function getPrompts() {
  * @return {void}
  */
 async function fetch() {
-  const saUrl = 'https://www.southwest.com';
+  try {
+    const browser = await puppeteer.launch();
 
-  const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36');
 
-  const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36');
-
-  const search = qs.stringify({
-    adultPassengersCount,
-    departureDate,
-    departureTimeOfDay: deptTime,
-    destinationAirportCode,
-    fareType: 'USD',
-    int: 'HOMEQBOMAIR',
-    originationAirportCode,
-    passengerType: 'ADULT',
-    reset: true,
-    returnDate,
-    returnTimeOfDay: retTime,
-    seniorPassengersCount: 0,
-    tripType: 'roundtrip',
-  });
-  const queryUrl = `https://www.southwest.com/air/booking/select.html?${search}`;
-
-  await page.goto(queryUrl, {waitUntil: 'networkidle2'});
-
-  const fairValueSelector = '.fare-button--value-total';
-
-  await page.waitForSelector(fairValueSelector);
-  await page.screenshot({ path: 'eg.png', fullPage: true });
-
-  const deptListHandle = await page.$('.search-results--container .container_standard:nth-child(2) .air-booking-select-price-matrix .transition-content ul');
-  const deptResults = await deptListHandle.$$eval('li .fare-button--value-total', nodes => nodes.map(n => n.innerText));
-
-  fares.outbound.push(...deptResults.map(res => parseInt(res, 10)).sort());
-
-  await deptListHandle.dispose();
-
-  const retListHandle = await page.$('.search-results--container .container_standard:nth-child(3) .air-booking-select-price-matrix .transition-content ul');
-  const retResults = await retListHandle.$$eval('li .fare-button--value-total', nodes => nodes.map(n => n.innerText));
-
-  fares.return.push(...retResults.map(res => parseInt(res, 10)).sort());
-
-  await retListHandle.dispose();
-
-  await browser.close();
-
-  let faresAreValid = true;
-
-  const lowestOutboundFare = Math.min(...fares.outbound);
-  const lowestReturnFare = Math.min(...fares.return);
-  const lowestRoundtrip = lowestOutboundFare + lowestReturnFare;
-
-  const outboundFareDiff = (prevLowestOutboundFare || lowestOutboundFare) - lowestOutboundFare;
-  const returnFareDiff = (prevLowestReturnFare || lowestReturnFare) - lowestReturnFare;
-  const roundtripFareDiff = (prevLowestRoundtrip || lowestRoundtrip) - lowestRoundtrip;
-
-  let outboundFareDiffString = '';
-  let returnFareDiffString = '';
-  let roundtripFareDiffString = '';
-
-  // Create a string to show the difference
-  if (!isNaN(outboundFareDiff) && !isNaN(returnFareDiff)) {
-
-    // Usually this is because of a scraping error
-    if (!isFinite(outboundFareDiff) || !isFinite(returnFareDiff)) {
-      faresAreValid = false
-    }
-
-    if (outboundFareDiff > 0) {
-      outboundFareDiffString = chalk.green(`(down \$${Math.abs(outboundFareDiff)})`)
-    } else if (outboundFareDiff < 0) {
-      outboundFareDiffString = chalk.red(`(up \$${Math.abs(outboundFareDiff)})`)
-    } else if (outboundFareDiff === 0) {
-      outboundFareDiffString = chalk.blue(`(no change)`)
-    }
-
-    if (returnFareDiff > 0) {
-      returnFareDiffString = chalk.green(`(down \$${Math.abs(returnFareDiff)})`)
-    } else if (returnFareDiff < 0) {
-      returnFareDiffString = chalk.red(`(up \$${Math.abs(returnFareDiff)})`)
-    } else if (returnFareDiff === 0) {
-      returnFareDiffString = chalk.blue(`(no change)`)
-    }
-
-    if (roundtripFareDiff > 0) {
-      roundtripFareDiffString = chalk.green(`(down \$${Math.abs(roundtripFareDiff)})`)
-    } else if (returnFareDiff < 0) {
-      roundtripFareDiffString = chalk.red(`(up \$${Math.abs(roundtripFareDiff)})`)
-    } else if (returnFareDiff === 0) {
-      roundtripFareDiffString = chalk.blue(`(no change)`)
-    }
-  }
-
-  if (faresAreValid) {
-    // Store current fares for next time
-    prevLowestOutboundFare = lowestOutboundFare;
-    prevLowestReturnFare = lowestReturnFare;
-    prevLowestRoundtrip = lowestRoundtrip;
-
-    // Do some Twilio magic (SMS alerts for awesome deals)
-    if (dealPriceThreshold && (lowestOutboundFare <= dealPriceThreshold || lowestReturnFare <= dealPriceThreshold)) {
-      const message = `Deal alert! Lowest fair has hit \$${lowestOutboundFare} (outbound) and \$${lowestReturnFare} (return)`;
-
-      // Party time
-      dashboard.log([
-        rainbow(message)
-      ]);
-
-      if (isTwilioConfigured) {
-        // sendTextMessage(message);
-      }
-    }
-
-    // Do some Twilio magic (SMS alerts for awesome deals)
-    if (dealPriceThresholdRoundTrip && lowestOutboundFare + lowestReturnFare <= dealPriceThresholdRoundTrip) {
-      const message = `Roundtrip deal alert! Lowest fair has hit \$${lowestOutboundFare + lowestReturnFare}`;
-
-      // Party time
-      dashboard.log([
-        rainbow(message)
-      ]);
-
-      if (isTwilioConfigured) {
-        // sendTextMessage(message);
-      }
-    }
-
-    dashboard.log([
-      `Lowest fair for an outbound flight is currently \$${[lowestOutboundFare, outboundFareDiffString].filter(i => i).join(" ")}`,
-      `Lowest fair for a return flight is currently \$${[lowestReturnFare, returnFareDiffString].filter(i => i).join(" ")}`,
-      `Lowest fair for roundtrip is currently \$${[lowestRoundtrip, roundtripFareDiffString].filter(i => i).join(" ")}`,
-    ]);
-
-    dashboard.plot({
-      outbound: lowestOutboundFare,
-      return: lowestReturnFare,
-      roundtrip: lowestRoundtrip,
+    const search = qs.stringify({
+      adultPassengersCount,
+      departureDate,
+      departureTimeOfDay: deptTime,
+      destinationAirportCode,
+      fareType: 'USD',
+      int: 'HOMEQBOMAIR',
+      originationAirportCode,
+      passengerType: 'ADULT',
+      reset: true,
+      returnDate,
+      returnTimeOfDay: retTime,
+      seniorPassengersCount: 0,
+      tripType: 'roundtrip',
     });
+    const queryUrl = `https://www.southwest.com/air/booking/select.html?${search}`;
+
+    await page.goto(queryUrl, {waitUntil: 'networkidle2'});
+
+    const fairValueSelector = '.fare-button--value-total';
+
+    await page.waitForSelector(fairValueSelector);
+    await page.screenshot({ path: 'eg.png', fullPage: true });
+
+    const deptListHandle = await page.$('.search-results--container .container_standard:nth-child(2) .air-booking-select-price-matrix .transition-content ul');
+    const deptResults = await deptListHandle.$$eval('li .fare-button--value-total', nodes => nodes.map(n => n.innerText));
+
+    fares.outbound.push(...deptResults.map(res => parseInt(res, 10)).sort());
+
+    await deptListHandle.dispose();
+
+    const retListHandle = await page.$('.search-results--container .container_standard:nth-child(3) .air-booking-select-price-matrix .transition-content ul');
+    const retResults = await retListHandle.$$eval('li .fare-button--value-total', nodes => nodes.map(n => n.innerText));
+
+    fares.return.push(...retResults.map(res => parseInt(res, 10)).sort());
+
+    await retListHandle.dispose();
+
+    await browser.close();
+
+    let faresAreValid = true;
+
+    const lowestOutboundFare = Math.min(...fares.outbound);
+    const lowestReturnFare = Math.min(...fares.return);
+    const lowestRoundtrip = lowestOutboundFare + lowestReturnFare;
+
+    const outboundFareDiff = (prevLowestOutboundFare || lowestOutboundFare) - lowestOutboundFare;
+    const returnFareDiff = (prevLowestReturnFare || lowestReturnFare) - lowestReturnFare;
+    const roundtripFareDiff = (prevLowestRoundtrip || lowestRoundtrip) - lowestRoundtrip;
+
+    let outboundFareDiffString = '';
+    let returnFareDiffString = '';
+    let roundtripFareDiffString = '';
+
+    // Create a string to show the difference
+    if (!isNaN(outboundFareDiff) && !isNaN(returnFareDiff)) {
+
+      // Usually this is because of a scraping error
+      if (!isFinite(outboundFareDiff) || !isFinite(returnFareDiff)) {
+        faresAreValid = false
+      }
+
+      if (outboundFareDiff > 0) {
+        outboundFareDiffString = chalk.green(`(down \$${Math.abs(outboundFareDiff)})`)
+      } else if (outboundFareDiff < 0) {
+        outboundFareDiffString = chalk.red(`(up \$${Math.abs(outboundFareDiff)})`)
+      } else if (outboundFareDiff === 0) {
+        outboundFareDiffString = chalk.blue(`(no change)`)
+      }
+
+      if (returnFareDiff > 0) {
+        returnFareDiffString = chalk.green(`(down \$${Math.abs(returnFareDiff)})`)
+      } else if (returnFareDiff < 0) {
+        returnFareDiffString = chalk.red(`(up \$${Math.abs(returnFareDiff)})`)
+      } else if (returnFareDiff === 0) {
+        returnFareDiffString = chalk.blue(`(no change)`)
+      }
+
+      if (roundtripFareDiff > 0) {
+        roundtripFareDiffString = chalk.green(`(down \$${Math.abs(roundtripFareDiff)})`)
+      } else if (returnFareDiff < 0) {
+        roundtripFareDiffString = chalk.red(`(up \$${Math.abs(roundtripFareDiff)})`)
+      } else if (returnFareDiff === 0) {
+        roundtripFareDiffString = chalk.blue(`(no change)`)
+      }
+    }
+
+    if (faresAreValid) {
+      // Store current fares for next time
+      prevLowestOutboundFare = lowestOutboundFare;
+      prevLowestReturnFare = lowestReturnFare;
+      prevLowestRoundtrip = lowestRoundtrip;
+
+      // Do some Twilio magic (SMS alerts for awesome deals)
+      if (dealPriceThreshold && (lowestOutboundFare <= dealPriceThreshold || lowestReturnFare <= dealPriceThreshold)) {
+        const message = `Deal alert! Lowest fair has hit \$${lowestOutboundFare} (outbound) and \$${lowestReturnFare} (return)`;
+
+        // Party time
+        dashboard.log([
+          rainbow(message)
+        ]);
+
+        if (isTwilioConfigured) {
+          sendTextMessage(message);
+        }
+      }
+
+      // Do some Twilio magic (SMS alerts for awesome deals)
+      if (dealPriceThresholdRoundTrip && lowestOutboundFare + lowestReturnFare <= dealPriceThresholdRoundTrip) {
+        const message = `Roundtrip deal alert! Lowest fair has hit \$${lowestOutboundFare + lowestReturnFare}`;
+
+        // Party time
+        dashboard.log([
+          rainbow(message)
+        ]);
+
+        if (isTwilioConfigured) {
+          sendTextMessage(message);
+        }
+      }
+
+      dashboard.log([
+        `Lowest fair for an outbound flight is currently \$${[lowestOutboundFare, outboundFareDiffString].filter(i => i).join(" ")}`,
+        `Lowest fair for a return flight is currently \$${[lowestReturnFare, returnFareDiffString].filter(i => i).join(" ")}`,
+        `Lowest fair for roundtrip is currently \$${[lowestRoundtrip, roundtripFareDiffString].filter(i => i).join(" ")}`,
+      ]);
+
+      dashboard.plot({
+        outbound: lowestOutboundFare,
+        return: lowestReturnFare,
+        roundtrip: lowestRoundtrip,
+      });
+    }
+
+    dashboard.render();
+
+    fares.outbound = [];
+    fares.return = [];
+    fares.roundtrip = [];
+  } catch (e) {
+    console.log(e);
   }
-
-  dashboard.render();
-
-  fares.outbound = [];
-  fares.return = [];
-  fares.roundtrip = [];
 
   setTimeout(fetch, interval * TIME_MIN);
 }
